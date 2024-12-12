8bd007a0bd6866ef045faa07eba280d26850a124
huzefa <huzefabiyawarwala045@gmail.com>
2024-12-12 19:35:08 +0530

    update add method to support custom delimiters of any length, also refactor the parsing logic into separate method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,12 +2,10 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?    
 
-    delimiter = ",|\n" # Default delimiters: comma or newline
+    # Default delimiters: comma or newline
+    delimiter = ",|\n" 
 
-    if numbers.start_with?("//")
-      header, numbers = numbers.split("\n", 2)
-      delimiter = Regexp.escape(header[2])
-    end
+    delimiter, numbers = parse_delimiter(numbers)
 
     # Split numbers and convert to integers
     numbers_array = numbers.split(/#{delimiter}/).map(&:to_i)
@@ -16,7 +14,24 @@ class StringCalculator
     negatives = numbers_array.select { |n| n < 0 }
     raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
 
-    # Calculate and return the sum
+    # Calculate and return the sum ignoring numbers greater than 1000
     numbers_array.reject{ |n| n > 1000 }.sum
+  end
+
+  private
+
+  def parse_delimiter(numbers) 
+    if numbers.start_with?("//") 
+      header, numbers = numbers.split("\n", 2) 
+      delimiter = if header.match(%r{//\[(.+)\]}) 
+        Regexp.escape(header.match(%r{//\[(.+)\]})[1]) 
+      else 
+        Regexp.escape(header[2]) 
+      end 
+
+      [delimiter, numbers] 
+    else 
+      [",|\n", numbers] 
+    end 
   end
 end
