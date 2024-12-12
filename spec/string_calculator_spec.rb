fca86d7022f4fdff5c83e2e51d867ed62540554e
huzefa <huzefabiyawarwala045@gmail.com>
2024-12-12 19:00:04 +0530

    added tests for raising exceptions on negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -31,5 +31,15 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises an exception for a single negative  number' do
+      calculator = StringCalculator.new
+      expect { calculator.add("1,-2,3") }.to raise_error("negatives not allowed: -2")
+    end
+
+    it 'raises an exception for multiple negative numbers' do 
+      calculator = StringCalculator.new
+      expect { calculator.add("1,-2,-3,4") }.to raise_error("negatives not allowed: -2, -3")
+    end
   end
 end
