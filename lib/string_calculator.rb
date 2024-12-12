6929918a408b7268948e0591be2028637a2aef96
huzefa <huzefabiyawarwala045@gmail.com>
2024-12-12 17:24:52 +0530

    update add method to handle newlines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,7 @@
 class StringCalculator
   def add(numbers)
-    return 0 if numbers.empty?
-    return numbers.to_i if !numbers.include?(',')
+    return 0 if numbers.empty?    
 
-    numbers.split(',').map(&:to_i).sum
+    numbers.gsub("\n", ",").split(',').map(&:to_i).sum
   end
 end
