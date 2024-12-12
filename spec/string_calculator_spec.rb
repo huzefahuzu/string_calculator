93c944165825e924a58f1394e1cbf3b75e7f580a
huzefa <huzefabiyawarwala045@gmail.com>
2024-12-12 19:15:22 +0530

    Add tests for ignoring numbers greater than 1000

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -41,5 +41,15 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect { calculator.add("1,-2,-3,4") }.to raise_error("negatives not allowed: -2, -3")
     end
+
+    it 'ignores numbers greater than 1000' do
+      calculator = StringCalculator.new
+      expect(calculator.add("2, 1001")).to eq(2)
+    end
+
+    it 'includes numbers less than or equal to 1000 in the sum' do
+      calculator = StringCalculator.new
+      expect(calculator.add("2,1000")).to eq(1002)
+    end
   end
 end
